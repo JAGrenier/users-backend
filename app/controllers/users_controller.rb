58dd61972bfd26088b81b704fa06fbdd48c87436
Kristine Du <kristine.a.du@gmail.com>
2020-09-17 15:02:19 -0400

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :update, :destroy]
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+
+        render json: @user
+    end
+
+    def update
+        @user.update(user_params)
+
+        render json: @user
+    end
+
+    def destroy
+        @user.destroy
+
+        render json: {message: "#{@user.username} deleted"}
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
